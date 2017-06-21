fb1828b9d2cde52d8cf1727196ca6eba8e842e82
Abishek <abishek@Abisheks-MacBook-Air.local>
2017-06-21 12:46:23 +0530

    Initial Commit

---
+++ Frainz/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Frainz
+//
+//  Created by Abishek on 6/21/17.
+//  Copyright © 2017 Abishek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
